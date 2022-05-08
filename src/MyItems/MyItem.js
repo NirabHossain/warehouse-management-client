efaccc52b283d2e0e6ceccaa2b998d3e87a7e9d6
NirabHossain <nirabamth@gmail.com>
2022-05-08 23:41:08 +0600

    final

--- src/MyItems/MyItem.js
+++ src/MyItems/MyItem.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
+import Bike from '../Pages/Home/Bike/Bike';
 
 const MyItems = () => {
     const [myItem, setMyItem] = useState([]);
@@ -10,10 +11,13 @@ const MyItems = () => {
         const url = `http://localhost:5000/bikes`;
         fetch(url).then(res => res.json()).then(data =>setMyItem(data))
     }, [email])
-
+    const userItem = myItem.filter(item=>item.email===user.email);
 return (
     <div>
         Total bikes: {myItem.length}
+        {
+            userItem.map(item=><Bike service={item}/>)
+        }
     </div>
 );
 };
