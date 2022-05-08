25711eb95a15aab044d3801d97cadf008422aeb3
NirabHossain <nirabamth@gmail.com>
2022-05-08 17:04:11 +0600

    not found

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
+        // Not found Image
         <div>
             <img src="/404.jpg" alt="" className='w-100 h-100' />
         </div>
